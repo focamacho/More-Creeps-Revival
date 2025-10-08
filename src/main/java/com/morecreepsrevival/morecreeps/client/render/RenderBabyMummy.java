aafe6994da81bad1e70f0aefae21d40b31d3b939
Renan Colombo <48870573+focamacho@users.noreply.github.com>
2025-10-08 10:12:03 -0300

    fix: mummies not having zombie hands

--- src/main/java/com/morecreepsrevival/morecreeps/client/render/RenderBabyMummy.java
+++ src/main/java/com/morecreepsrevival/morecreeps/client/render/RenderBabyMummy.java
@@ -2,10 +2,11 @@ package com.morecreepsrevival.morecreeps.client.render;
 
 import com.morecreepsrevival.morecreeps.common.entity.EntityBabyMummy;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelZombie;
 import net.minecraft.client.renderer.entity.RenderManager;
 
 public class RenderBabyMummy<T extends EntityBabyMummy> extends RenderCreep<T> {
     public RenderBabyMummy(RenderManager renderManager) {
-        super(renderManager, new ModelBiped(), 0.5f);
+        super(renderManager, new ModelZombie(), 0.5f);
     }
 }
